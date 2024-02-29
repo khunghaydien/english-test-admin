2e66277f4cbd72023ee5779e65672088a50041f6
DatPH <datph@mor.com.vn>
2024-02-29 14:41:00 +0700

    change language

--- app/[language]/(login)/login/page.tsx
+++ app/[language]/(login)/login/page.tsx
@@ -1,18 +1,13 @@
 "use client";
-import { getDictionary } from "@/language/get-dictionary";
-import { Locale } from "@/language/i18n-config";
 import LoginForm from "@/modules/login/LoginForm";
 import { useRouter } from "next/navigation";
-const LoginPage = async ({
-  params: { language },
-}: {
-  params: { language: Locale };
-}) => {
+import { useMessages } from "next-intl";
+const LoginPage = () => {
   const router = useRouter();
-  const dictionary = await getDictionary(language);
+  const t = useMessages();
   return (
     <div className="login-form border p-5 rounded-lg">
-      <div className="text-center font-bold">{dictionary.LB_LOGIN}</div>
+      <div className="text-center font-bold">{t.LB_LOGIN.toString()}</div>
       <LoginForm
         onLogin={() => router.push("/exams/dashboard")}
         onForgotPassword={() => router.push("/forgot-password")}
